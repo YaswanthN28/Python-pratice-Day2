48937e82016510d15fd0ef5b0e1070e2b6f92d48
Yaswanth <yash@gmail.com>
2026-08-11 10:34:47 +0530

    slicing

---
+++ question5.py
@@ -0,0 +1,9 @@
+message=input("Enter message:")
+
+print("First 5 character:",message[:5])
+print("Last 5 characcter:",message[-5:])
+print("character from index 2 to 7 :",message[2:8])
+print("Every second character:",message[::2])
+print("Message in Reverse:",message[::-1])
+print("message without first and last character:",message[1:-1])
+
